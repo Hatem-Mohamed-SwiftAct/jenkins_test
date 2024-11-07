661dc950167cbc36f49a7d7de80b235fe3adf692
Hatem Mohamed <hatem.mohamed@swift-act.com>
2024-11-07 10:40:57 +0200

    Test_one

---
+++ first_test.py
@@ -0,0 +1,11 @@
+#------------------------------- Import Libraries -------------------------------#
+# Import the PyTest Library
+import pytest
+
+# Import time module to add delays (if needed)
+import time
+
+#----------------------------- Sequence 1 -------------------------------#
+def test_seq_1():
+    
+    assert True
